9b6c9b497a84e4eb79a9b7d7b08b4ec9590c045b
B.Johnston <brandon@coffeenerds.io>
2020-12-09 14:53:19 +0100

    Chapter 4 Template Conditionals, Loops & Events - v-for with :key loop (twott content)

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  min-height: 100vh;
+  min-height: 98vh;
   background-color: #F3F5FA;
 }
 </style>
