12945eccd1f301767b18d8f1da58b8f84da18740
B.Johnston <brandon@coffeenerds.io>
2020-12-16 09:33:51 +0100

    Chapter 6: Dynamic Styling, SCSS, & Global Styles - Add npm packages for scss - refactor styling into scss with nesting, also use "scoped" to make sure styles only apply to the components where they are declared

--- src/components/TwootItem.vue
+++ src/components/TwootItem.vue
@@ -32,23 +32,22 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .twoot-item {
   padding: 20px;
-  margin: 6px 0 0 0;
   background-color: white;
   border-radius: 5px;
   border: 1px solid #DFE3E8;
   box-sizing: border-box;
   cursor: pointer;
   transition: all 0.25s ease;
-}
 
-.twoot-item:hover {
-  transform: scale(1.1, 1.1);
-}
+  &:hover {
+    transform: scale(1.1, 1.1);
+  }
 
-.twoot-item__user {
-  font-weight: bold;
+  .twoot-item__user {
+    font-weight: bold;
+  }
 }
 </style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -101,39 +101,44 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .user-profile {
   display: grid;
   grid-template-columns: 1fr 3fr;
   padding: 50px 5%;
-}
 
-.user-profile__user-panel {
-  display: flex;
-  flex-direction: column;
-  margin-right: 50px;
-  padding: 20px;
-  background-color: white;
-  border-radius: 5px;
-  border: 1px solid #DFE3E8;
-}
+  .user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
 
-h1 {
-  margin: 0;
-}
+    h1 {
+      margin: 0;
+    }
 
-.user-profile__admin-badge {
-  background: cornflowerblue;
-  color: white;
-  border-radius: 5px;
-  margin-right: auto;
-  padding: 0 10px;
-  font-weight: bold;
-}
+    .user-profile__admin-badge {
+      background: cornflowerblue;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      padding: 0 10px;
+      font-weight: bold;
+    }
 
-.user-profile__create-twoot {
-  padding-top: 20px;
-  display: flex;
-  flex-direction: column;
+    .user-profile__create-twoot {
+      padding-top: 20px;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+  .user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+  }
 }
 </style>
