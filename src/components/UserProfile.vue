7937527541deb47152351edea013b4649fcba487
B.Johnston <brandon@coffeenerds.io>
2020-12-16 09:41:59 +0100

    Chapter 6: Dynamic Styling, SCSS, & Global Styles - Dynamic styling when character limit exceeded (:class)

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -8,8 +8,10 @@
       <div class="user-profile__follower-count">
         <strong>Followers: </strong> {{ followers }}
       </div>
-      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
-        <label for="newTwoot"><strong>New Twoot</strong></label>
+      <form class="user-profile__create-twoot"
+            @submit.prevent="createNewTwoot"
+            :class="{'exceeded': newTwootCharacterCount > 180}">
+        <label for="newTwoot"><strong>New Twoot</strong> ({{ newTwootCharacterCount }}/180)</label>
         <textarea id="newTwoot" rows="4" v-model="newTwootContent"/>
 
         <div class="user-profile__create-twoot-type">
@@ -74,8 +76,8 @@ export default {
     }
   },
   computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
+    newTwootCharacterCount() {
+      return this.newTwootContent.length
     }
   },
   methods: {
@@ -133,6 +135,17 @@ export default {
       padding-top: 20px;
       display: flex;
       flex-direction: column;
+
+      &.exceeded {
+        color: red;
+        border-color: red;
+
+        button {
+          background-color: red;
+          border: none;
+          color: white;
+        }
+      }
     }
   }
 
