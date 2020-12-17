d60f6c7107e7d8e5c9f731b4cd7f1f7ac339815d
B.Johnston <brandon@coffeenerds.io>
2020-12-17 15:29:13 +0100

    Chapter 8: Routing With Vue Router 4 - Add admin view with route guard (router.beforeEach)

---
+++ src/views/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="admin"></div>
+  <h1>Admin</h1>
+</template>
+
+<script>
+export default {
+  name: "Admin"
+}
+</script>
+
+<style scoped>
+.admin {
+  padding: 50px 5%;
+}
+</style>
